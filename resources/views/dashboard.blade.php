98a2f6066701572f64ed40bd812a3d5819138e46
Maximilian Schwarzmüller <mblacky0@gmail.com>
2016-02-11 13:11:48 +0100

    Added dashboard HTML and styling

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,1 +1,44 @@
-<h1>The Dashboard</h1>
+@extends('layouts.master')
+
+@section('content')
+    <section class="row new-post">
+        <div class="col-md-6 col-md-offset-3">
+            <header><h3>What do you have to say?</h3></header>
+            <form action="">
+                <div class="form-group">
+                    <textarea class="form-control" name="new-post" id="new-post" rows="5" placeholder="Your Post"></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary">Create Post</button>
+            </form>
+        </div>
+    </section>
+    <section class="row posts">
+        <div class="col-md-6 col-md-offset-3">
+            <header><h3>What other people say...</h3></header>
+            <article class="post">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis, eligendi repellat? Labore nemo nostrum, provident quam quasi velit? Accusamus aut beatae eius ex labore magni minus obcaecati quia quisquam sit.</p>
+                <div class="info">
+                    Posted by Max on 12 Feb 2016
+                </div>
+                <div class="interaction">
+                    <a href="#">Like</a> |
+                    <a href="#">Dislike</a> |
+                    <a href="#">Edit</a> |
+                    <a href="#">Delete</a>
+                </div>
+            </article>
+            <article class="post">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis, eligendi repellat? Labore nemo nostrum, provident quam quasi velit? Accusamus aut beatae eius ex labore magni minus obcaecati quia quisquam sit.</p>
+                <div class="info">
+                    Posted by Max on 12 Feb 2016
+                </div>
+                <div class="interaction">
+                    <a href="#">Like</a> |
+                    <a href="#">Dislike</a> |
+                    <a href="#">Edit</a> |
+                    <a href="#">Delete</a>
+                </div>
+            </article>
+        </div>
+    </section>
+@endsection
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,6 +3,7 @@
 <head>
     <title>@yield('title')</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ URL::to('src/css/main.css') }}">
 </head>
 <body>
     @include('includes.header')
